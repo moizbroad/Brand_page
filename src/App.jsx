cd341559fee79a3853cc835f9794e0ac7c42fdc7
moizbroad <moizbroadthinker543@gmail.com>
2023-07-08 03:45:57 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navigation from "./Component/Navigation";
+import HeroSection  from "./Component/Hero";
+const App = () => {
+    return <div>
+       <Navigation/>
+       <HeroSection/>
+
+    </div>
+}
+
+export default App;  // access outide 
